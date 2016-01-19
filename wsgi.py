9ba8a83462af0bf876996f9f17debb117d1d9254
Roman Salin <romansalin1990@gmail.com>
2016-01-19 17:48:15 +0300

    Add deploy configs

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from testrail_reporting import create_app
 
+app = create_app('production')
+
 if __name__ == '__main__':
-    app = create_app('production')
     app.run(host='0.0.0.0')
